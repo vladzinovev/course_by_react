a5de9eba96e94b175748277e3e5e22bde046d474
Vladislav <vladozdeveloper@gmail.com>
2022-04-26 18:35:45 +0300

    solved the test

---
+++ scripts/lesson11.js
@@ -0,0 +1,57 @@
+//lesson 11 (data types)
+
+let number = 4.6;
+
+console.log(-4/0);
+console.log('string' *9);
+
+const person = `5`;
+
+const bool = false;
+
+//console.log(something);
+
+let und;
+console.log(und);
+
+
+const obj = {
+    name: "John",
+    age:25,
+    isMarried:false,
+    4:no,
+    abc:{
+        df: [{},{}],
+        def:{
+
+        }
+    }
+};
+
+console.log(obj.name);
+console.log(obj["age"]);
+console.log(obj["name"]);
+console.log(obj[4]);
+
+obj.b = '1234';
+console.log(obj.b);
+
+obj['c'] = 'man';
+console.log(obj.c);
+
+
+let arr = ['plum.png', 'orange.png', 6, 'apple.png', {}, [] ];
+console.log(arr[0]);
+
+
+//test
+const storeName='Zara';
+const storeDescription={
+    budget:10000,
+    employees:['John', 'Max', 'Alice'],
+    products:{
+        apple:320,
+        orange:280
+    },
+    open:true
+}
---
+++ scripts/lesson9.js
@@ -0,0 +1,21 @@
+//lesson 9 (variables)
+"use strict";
+
+let number = 5;
+
+const leftBorderWidth = 1;
+
+number = 10;
+console.log(number);
+
+const obj = {
+    a:20
+};
+
+obj.a=10;
+console.log(obj);
+
+//test
+const userName = "John";
+let userNumber = 25;
+userNumber = 24;
