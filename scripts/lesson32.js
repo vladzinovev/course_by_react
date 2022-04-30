6becddf36a18088608e99be00590d5464d75e932
Vladislav <vladozdeveloper@gmail.com>
2022-04-30 12:13:26 +0300

    completed lesson 32

---
+++ scripts/lesson32.js
@@ -0,0 +1,66 @@
+// lesson 32 (objects)
+"use strict"
+
+
+const options={
+    name: 'test',
+    width: 1024,
+    height: 1024,
+    colors: {
+        border: 'black',
+        bg:'red'
+    }
+};
+
+console.log(Object.keys(options)); //[ 'name', 'width', 'height', 'colors' ]
+console.log(Object.keys(options).length); //4
+
+console.log(options.name);
+
+
+console.log(options["colors"]["border"]);
+const {border , bg} = options.colors;
+console.log(border);
+
+delete options.name;
+console.log(options); //{ width: 1024, height: 1024, colors: { border: 'black', bg: 'red' } }
+
+
+for (let key in options){
+    if (typeof(options[key]) === 'object'){
+        for (let i in options[key]){
+            console.log(`Свойство ${i} имеет значение ${options[key][i]}`)
+        }
+    }else{
+        console.log(`Свойство ${key} имеет значение ${options[key]}`);
+    }
+}
+
+
+
+let counter;
+for (let key in options){
+    if (typeof(options[key]) === 'object'){
+        for (let i in options[key]){
+            console.log(`Свойство ${i} имеет значение ${options[key][i]}`)
+        }
+    }else{
+        console.log(`Свойство ${key} имеет значение ${options[key]}`);
+        counter++;
+    }
+}
+
+
+const option1={
+    name: 'test',
+    width: 1024,
+    height: 1024,
+    colors: {
+        border: 'black',
+        bg:'red'
+    },
+    makeTest:function(){
+        console.log("Test");
+    }
+};
+options.makeTest(); //test
