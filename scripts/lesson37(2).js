be357b0bd02b3a0e42944770fa63c59cd2d6c094
Vladislav <vladozdeveloper@gmail.com>
2022-05-04 19:29:23 +0300

    completed lesson39

---
+++ scripts/lesson37(2).js
@@ -0,0 +1,26 @@
+// lesson 37(2) (practice)
+"use strict"
+const students = ['Peter', 'Andrew', 'Ann', 'Mark', 'Josh', 'Sandra', 'Cris', 'Bernard', 'Takesi', 'Sam'];
+
+function sortStudentsByGroups(arr) {
+    arr.sort();
+    let arrNew1 =[];
+    let arrNew2 =[];
+    let arrNew3 =[];
+    let arrNew4 =[];
+
+    for (let i = 0; i < arr.length; i++) {
+        if (i < 3) {
+            arrNew1.push(arr[i]);
+        } else if (i < 6) {
+            arrNew2.push(arr[i]);
+        } else if (i < 9) {
+            arrNew3.push(arr[i]);
+        } else {
+            arrNew4.push(arr[i]);
+        }
+    }
+    console.log([arrNew1,arrNew2,arrNew3,`Оставшиеся студенты: ${arrNew4.length === 0 ? '-' : arrNew4.join(', ')}`])
+    return [arrNew1,arrNew2,arrNew3,`Оставшиеся студенты: ${arrNew4.length === 0 ? '-' : arrNew4.join(', ')}`];
+};
+sortStudentsByGroups(students);
---
+++ scripts/lesson39.js
@@ -0,0 +1,5 @@
+// lesson 39 (dynamic typing)
+"use strict"
+
+
+
