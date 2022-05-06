a14ef20ff0e9c1e8b20418a8000ee43ff01cd2e2
Vladislav <vladozdeveloper@gmail.com>
2022-05-06 16:32:14 +0300

    created elements on display for lesson42

---
+++ scripts/lesson42.js
@@ -0,0 +1,35 @@
+// lesson 42 (getting elements from the page)
+"use strict"
+
+//получаем эл через id
+const box = document.getElementById('box'); 
+
+//получаем все эл button
+const btns = document.getElementsByTagName('button');
+//получаем второй эл button из всех
+const btn = document.getElementsByTagName('button')[2];
+//получаем второй эл button из всех
+const btnses = document.getElementsByTagName('button');
+console.log(btnses[2])
+
+//получаем все элементы через class
+const circles = document.getElementsByClassName('circle');
+
+//получаем все элементы через selector
+const hearts = document.querySelectorAll('.heart');
+
+hearts.forEach(item=>{
+    console.log(item);
+});
+//получаем первый элементы на странице по selector
+const oneHeart = document.querySelector('.heart');
+
+
+
+
+
+
+
+
+
+
