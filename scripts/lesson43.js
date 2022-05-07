8934e7529a8957a3f089794774f6fa21a2d79556
Vladislav <vladozdeveloper@gmail.com>
2022-05-07 10:22:31 +0300

    completed lesson 43

---
+++ scripts/lesson43.js
@@ -0,0 +1,100 @@
+// lesson 43 (actions with elements on the page)
+"use strict"
+
+
+const box = document.getElementById('box'); 
+const btns = document.getElementsByTagName('button');
+const circles = document.getElementsByClassName('circle');
+const hearts = document.querySelectorAll('.heart');
+const oneHeart = document.querySelector('.heart');
+//или
+const hearts = wrapper.querySelectorAll('.heart');
+const oneHeart = wrapper.querySelector('.heart');
+
+
+box.style.backgroundColor='blue';
+box.style.width='500px';
+//или
+box.style.cssText='background-color:blue; width:500px';
+
+
+btns[1].style.borderRadius='100%';
+circles[0].style.backgroundColor='red';
+
+/* 
+for (let i=0; i<hearts.length;i++){
+    hearts[i].style.backgroundColor='black';
+}
+*/
+
+//item-каждый элемент, второй аргумент -номер по порядку, третий - ссылка на массив
+hearts.forEach(item=>{
+    item.style.backgroundColor='yellow';
+});
+
+//создать элемент
+const div = document.createElement('div');
+
+//const text = document.createTextNode('Тут был я')
+
+//добавили класс black
+div.classList.add('black');
+
+//добавить элемент в конец родителя
+document.body.append(div);
+//или старый вариант (через родителя)
+//wrapper.appendChild(div);
+
+//добавить элемент в wrapper в конец родителя
+document.querySelector('.wrapper').append(div);
+//или
+const wrapper = document.querySelector('.wrapper');
+wrapper.append(div);
+
+//добавить элемент в wrapper в начало родителя
+wrapper.prepend(div);
+
+
+//вставить эл перед каким то определенным эл
+hearts[0].before(div);
+//вставить эл после какого то определенного эл
+hearts[0].after(div);
+//или старый вариант вставить (что, перед каким) (через родителя)
+//wrapper.insertBefore(div,hearts[0]);
+
+//удалить эл
+circles[0].remove();
+// или старый вариант удалить (через родителя)
+//wrapper.removeChild(hearts[1])
+
+//заменить эл каким то эл
+hearts[0].replaceWith(circles[0]);
+//или старый вриант заменить эл каким то эл (какой , перед каким)
+wrapper.replaceChild(circles[0], hearts[0]);
+
+
+//вписать текст html
+div.innerHTML="Hello world";
+div.innerHTML="<h1>Hello world</h1>";
+//или только с текстом
+div.textContent="Hello";
+
+
+
+//вставить кусочек html кода перед или после эл (спец слово, что вставляем)
+//спец слово:
+// afterbegin вставялет в начало
+//afterend вставяляет после эл
+// beforebegin вставляет перед элементом
+// beforeend вставляет в конец нашего эл
+div.insertAdjacentHTML('beforebegin', '<h2>Hello</h2>');
+
+
+
+
+
+
+
+
+
+
