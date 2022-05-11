fcf9bcc7d53b6d08b1c8f88e05084581638aa017
Vladislav <vladozdeveloper@gmail.com>
2022-05-11 15:25:14 +0300

    practice solution from the teacher in lesson 47(1)

--- scripts/lesson47(1).js
+++ scripts/lesson47(1).js
@@ -17,16 +17,30 @@ function factorial(n) {
     }
     return n;
 };
-
-
 console.log(factorial(5));
 
 
 
 
+/* 
+function factorial(n) {
+    if (typeof(n) !== 'number' || !Number.isInteger(n)) {
+        return "Ошибка, проверьте данные";
+    }
 
+    if (n >= 1) {
+        return n * factorial(n - 1);
+    } else {
+        return 1;
+    }
 
+    // Более короткий вариант, который вы можете встретить
+    // Но не учитывает отрицательные значения
+    return n ? n * factorial(n - 1) : 1;
+}
 
+factorial(5)
+ */
 
 
 
