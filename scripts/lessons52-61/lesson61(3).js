99e94cb9885fc978ad9122862f5ab3dbfccf33a6
Vladislav <vladozdeveloper@gmail.com>
2022-05-21 23:57:57 +0300

    solving a task for practice 61(3) from a teacher

--- scripts/lessons52-61/lesson61(3).js
+++ scripts/lessons52-61/lesson61(3).js
@@ -9,10 +9,22 @@ function deepCount(a){
         if (Array.isArray(a[i])) {
             count += deepCount(a[i]);
         }
-
     }
     return count;
 }
 let arr = [1,2,3,[5,[6]],10,11,[12,[13]]];
 console.log(deepCount(arr));
 
+
+//решение преподавателя
+// Вариант с циклом
+function deepCount(a){
+    let count = a.length;
+    for (let i=0; i<a.length; i++) if (Array.isArray(a[i])) count += deepCount(a[i]);
+    return count;
+}
+
+// Вариант с методом reduce
+function deepCount(a){
+    return a.reduce((s,e)=>s+(Array.isArray(e) ? deepCount(e) : 0),a.length);
+}
