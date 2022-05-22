71006e8992087a0584f163c202b48dfb02e0c4ae
Vladislav <vladozdeveloper@gmail.com>
2022-05-23 00:24:49 +0300

    completed created tabs for site in the lesson64

---
+++ scripts/lessons63-80/lesson64/js/lesson64.js
@@ -0,0 +1,45 @@
+window.addEventListener('DOMContentLoaded',()=>{
+
+    const tabs = document.querySelectorAll('.tabheader__item'),
+          tabsContent = document.querySelectorAll('.tabcontent'),
+          tabsParent = document.querySelector('.tabheader__items');
+
+    //сначала скрываем весь контент у табов      
+    function hideTabContent(){
+        tabsContent.forEach(item=>{
+            //item.style.display='none';
+            //или
+            item.classList.add('hide');
+            item.classList.remove('show','fade');
+        });
+        tabs.forEach(item=>{
+            item.classList.remove('tabheader__item_active');
+        });
+    }
+
+    //функция пл показу контента и активации класса активности
+    function showTabContent(i){
+        //tabsContent[i].style.display='block';
+        //или
+        tabsContent[i].classList.add('show','fade');
+        tabsContent[i].classList.remove('hide');
+        tabs[i].classList.add('tabheader__item_active');
+    }
+
+    hideTabContent();
+    showTabContent(0);
+
+    tabsParent.addEventListener('click',(event)=>{
+        const target = event.target;
+
+        if(target && target.classList.contains('tabheader__item')){
+            tabs.forEach((item,i)=>{
+                if(target==item){
+                    hideTabContent();
+                    showTabContent(i);
+                }
+            });
+        }
+    });
+
+});
