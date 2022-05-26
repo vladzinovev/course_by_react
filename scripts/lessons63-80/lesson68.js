53189cdf936951274a48292b3e9500458fe83881
Vladislav <vladozdeveloper@gmail.com>
2022-05-27 00:01:08 +0300

    completed lesson 68

--- scripts/lessons63-80/lesson68.js
+++ scripts/lessons63-80/lesson68.js
@@ -21,6 +21,8 @@ console.log(now.getTimezoneOffset());//-180 (3 часа)
 console.log(now.getTime());//1653598336202
 
 const my = new Date('2022-10-10');
+//тоже самое
+//new Date.parse('2022-10-10');
 console.log(my);//2022-10-10T00:00:00.000Z
 
 const myDate = new Date(2022, 5, 1, 20);
@@ -33,3 +35,20 @@ console.log(myDate2);//1970-01-01T00:00:00.000Z (начало отсчета)
 //передаем в секундах
 const myDate3 = new Date(-10);
 console.log(myDate3);//1969-12-31T23:59:59.990Z (от начала отсчета)
+
+
+
+//set
+const nows = new Date();
+console.log(nows.setHours(18));//1653580528649
+console.log(nows);//2022-05-26T15:55:28.649Z
+console.log(nows.setHours(18,40));//1653580528649
+console.log(nows);//2022-05-26T15:40:28.649Z
+
+
+let start =new Date();
+for (let i=0; i<10000; i++){
+    let some =i**3;
+}
+let end =new Date();
+console.log(end-start);//483
