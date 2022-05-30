fc91f60215ca3e83bc04c78936e19c265fbca8cc
Vladislav <vladozdeveloper@gmail.com>
2022-05-30 20:57:21 +0300

    added html plus css for lesson 72

--- scripts/lessons7-51/lesson32.js
+++ scripts/lessons7-51/lesson32.js
@@ -1,5 +1,5 @@
 // lesson 32 (objects)
-"use strict"
+"use strict";
 
 
 const options={
@@ -12,16 +12,16 @@ const options={
     }
 };
 
-
+console.log(options);//{name: 'test', width: 1024, height: 1024, colors: { border: 'black', bg: 'red' }}
 console.log(Object.keys(options)); //[ 'name', 'width', 'height', 'colors' ]
 console.log(Object.keys(options).length); //4
 
-console.log(options.name);
+console.log(options.name);//test
 
 
-console.log(options["colors"]["border"]);
+console.log(options['colors']['border']);//black
 const {border , bg} = options.colors;
-console.log(border);
+console.log(border);//black
 
 delete options.name;
 console.log(options); //{ width: 1024, height: 1024, colors: { border: 'black', bg: 'red' } }
@@ -30,7 +30,7 @@ console.log(options); //{ width: 1024, height: 1024, colors: { border: 'black', 
 for (let key in options){
     if (typeof(options[key]) === 'object'){
         for (let i in options[key]){
-            console.log(`Свойство ${i} имеет значение ${options[key][i]}`)
+            console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
         }
     }else{
         console.log(`Свойство ${key} имеет значение ${options[key]}`);
@@ -43,7 +43,7 @@ let counter;
 for (let key in options){
     if (typeof(options[key]) === 'object'){
         for (let i in options[key]){
-            console.log(`Свойство ${i} имеет значение ${options[key][i]}`)
+            console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
         }
     }else{
         console.log(`Свойство ${key} имеет значение ${options[key]}`);
