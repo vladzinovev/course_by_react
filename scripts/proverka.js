1fdc33558850b138c677c0480e9e16019dc00da4
Vladislav <vladozdeveloper@gmail.com>
2022-06-03 17:24:50 +0300

    tested in the practice new files and methods

--- scripts/proverka.js
+++ scripts/proverka.js
@@ -23,3 +23,23 @@ Object.defineProperty(object1, 'property1', {
 
 console.log(object1.property1);
 // expected output: 42
+
+
+const now = new Date();
+console.log(now);//2022-05-26T20:41:37.326Z
+//получаем год
+console.log(now.getFullYear());//2022
+//получаем месяц
+console.log(now.getMonth());//4
+//получаем день
+console.log(now.getDate());//26
+//получаем день недели начиная с вс
+console.log(now.getDay());//4 четверг
+//получаем часы 
+console.log(now.getHours());//23
+//получаем часы пояса
+console.log(now.getUTCHours());//20
+//получаем разницы между часовым поясом и UTC
+console.log(now.getTimezoneOffset());//-180 (3 часа)
+//получаем сколько прошло секунд от 1970 года
+console.log(now.getTime());//1653598336202
