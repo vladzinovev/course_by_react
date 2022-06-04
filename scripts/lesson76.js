48873be18c7ae11a402cef162ce8c598bfa09e4d
Vladislav <vladozdeveloper@gmail.com>
2022-06-04 08:55:43 +0300

    context this in the constructor

--- scripts/lesson76.js
+++ scripts/lesson76.js
@@ -37,7 +37,16 @@ object.sum();//при use strict будет undefind
 
 
 
-
+//3)this в конструкторах и классах - новый экземпляр обьекта
+function User(name,id){
+    this.name = name;
+    this.id=id;
+    this.human=true;
+    this.hello=function(){
+        console.log(`Hello ${this.name}`);
+    };
+}
+const ivan = new User('Ivan',28);
 
 
 
