9e5ff7f43f69009fc0616b8feeba3958be4a0645
Vladislav <vladozdeveloper@gmail.com>
2022-06-05 23:18:10 +0300

    created new class

---
+++ scripts/lesson77.js
@@ -0,0 +1,34 @@
+// lesson 75 (classes)
+"use strict";
+
+
+
+class Rectangle {
+    constructor(height,width){
+        this.height=height;
+        this.width=width;
+    }
+
+    calcArea(){
+        return this.height*this.width;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
