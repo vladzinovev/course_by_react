c7ca3d347a9c8017362ba22ddb862084bbc3a867
Vladislav <vladozdeveloper@gmail.com>
2022-06-05 23:31:42 +0300

    inherit class

--- scripts/lesson77.js
+++ scripts/lesson77.js
@@ -20,9 +20,21 @@ const long =new Rectangle (20,100);
 console.log(long.calcArea());//2000
 
 
+//наследуем клаcс
+class ColoredRectangleWithText extends Rectangle {
+    constructor(height,width,text,bgColor){
+        super(height,width);//super означает, что this.height=height; и this.width=width;
+        this.text=text;
+        this.bgColor=bgColor;
+    }
+    showMyProps(){
+        console.log(`Текст: ${this.text}, цвет: ${this.bgColor}`);
+    }
+}
 
-
-
+const div = new ColoredRectangleWithText(25, 10, 'Hello world','red');
+div.showMyProps();//Текст: Hello world, цвет: red
+console.log(div.calcArea());//250
 
 
 
