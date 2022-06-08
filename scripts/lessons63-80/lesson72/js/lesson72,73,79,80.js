09a49d74d971739bcc08365a96cd4b1a833f1c0e
Vladislav <vladozdeveloper@gmail.com>
2022-06-08 23:31:45 +0300

    completed lesson 80

--- scripts/lessons63-80/lesson72/js/lesson72,73,79,80.js
+++ scripts/lessons63-80/lesson72/js/lesson72,73,79,80.js
@@ -180,7 +180,7 @@ window.addEventListener('DOMContentLoaded', function() {
 
     // Используем классы для создание карточек меню
     //урок 79
-    class MenuCard {
+/*  class MenuCard {
         constructor(src, alt, title, descr, price, parentSelector) {
             this.src = src;
             this.alt = alt;
@@ -208,6 +208,48 @@ window.addEventListener('DOMContentLoaded', function() {
                         <div class="menu__item-cost">Цена:</div>
                         <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
                     </div>
+                </div>
+            `;
+            this.parent.append(element);
+        }
+    }
+*/
+    //урок 80
+    class MenuCard {
+        constructor(src, alt, title, descr, price, parentSelector, ...classes) {
+            this.src = src;
+            this.alt = alt;
+            this.title = title;
+            this.descr = descr;
+            this.price = price;
+            this.classes = classes;
+            this.parent = document.querySelector(parentSelector);
+            this.transfer = 27;
+            this.changeToUAH(); 
+        }
+
+        changeToUAH() {
+            this.price = this.price * this.transfer; 
+        }
+
+        render() {
+            const element = document.createElement('div');
+
+            if (this.classes.length === 0) {
+                this.classes = "menu__item";
+                element.classList.add(this.classes);
+            } else {
+                this.classes.forEach(className => element.classList.add(className));
+            }
+
+            element.innerHTML = `
+                <img src=${this.src} alt=${this.alt}>
+                <h3 class="menu__item-subtitle">${this.title}</h3>
+                <div class="menu__item-descr">${this.descr}</div>
+                <div class="menu__item-divider"></div>
+                <div class="menu__item-price">
+                    <div class="menu__item-cost">Цена:</div>
+                    <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
                 </div>
             `;
             this.parent.append(element);
