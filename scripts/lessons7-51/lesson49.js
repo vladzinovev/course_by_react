a7fb921a546e3b91f64fbe7110a6c2cbf9027f0e
Vladislav <vladozdeveloper@gmail.com>
2022-06-10 17:58:24 +0300

    corrected comments and text in the lesson 49

--- scripts/lessons7-51/lesson49.js
+++ scripts/lessons7-51/lesson49.js
@@ -3,34 +3,34 @@
 
 //touchstart  когда коснулись пальцем элемента
 //touchmove   когда после косания элемент начинает двигаться
-//touchmove   когда палец оторвался от элемента
-//touchenter  когда палец c эл достиг определенной области
-//touchleave  когда палец ушел за пределы эл
-//touchleave  когда палец ушел за пределы экрана
+//touchend   когда палец оторвался от элемента
+//touchenter  когда палец наскакивает на эл
+//touchleave  когда палец вышел за пределы элементы
+//touchcancel  когда палец вышел за пределы экрана
 
 window.addEventListener('DOMContentLoaded', () => {
     const box = document.querySelector('.box');
     
-    //нажал пальцем
+    //нажали пальцем на элемент (коснулись)
     box.addEventListener('touchstart',(e)=>{
         e.preventDefault();
         console.log('start');
         console.log(e.touches); //информация о пальце(ах) коснувшемся
     });
 
-    //водим по эл пальцем
+    //водим по элементу пальцем
     box.addEventListener('touchmove',(e)=>{
         e.preventDefault();
         console.log('move');
     });
 
-    //вотпустил палец
+    //тпустили палец
     box.addEventListener('touchend',(e)=>{
         e.preventDefault();
         console.log('End');
     });
 
-    //раюотаем с координатами пальцев
+    //работаем с координатами пальцев
     box.addEventListener('touchmove',(e)=>{
         e.preventDefault();
         console.log(e.targetTouches[0].pageX);
