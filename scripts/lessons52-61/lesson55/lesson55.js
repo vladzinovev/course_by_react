a1b745e4d41ac072e33c458f57dac845607fab66
Vladislav <vladozdeveloper@gmail.com>
2022-06-11 23:10:38 +0300

    corrected files for lesson 54

--- scripts/lessons52-61/lesson55/lesson55.js
+++ scripts/lessons52-61/lesson55/lesson55.js
@@ -1,28 +1,37 @@
+// lesson 55 (живые коллекции и полезные методы)
 'use strict';
 
 const boxesSelector = document.querySelectorAll('.box');
 const boxesClass = document.getElementsByClassName('box');
 
 
-console.log(boxesSelector);
-console.log(boxesClass);
+console.log(boxesSelector);//NodeList(3) [div.box, div.box.this, div.box]
+console.log(boxesClass);//HTMLCollection(3) [div.box, div.box.this, div.box]
 console.log(document.body.children);
 
 boxesSelector[0].remove();
 boxesClass[0].remove();
 
-console.log(boxesSelector);//будет три эл, так как это копия эл
-console.log(boxesClass);//будет один эл так как два эл мы удалили
-console.log(Array.from(boxesClass));//будет один эл, так как статичный массив
+//будет три эл, так как это копия эл
+console.log(boxesSelector);//NodeList(3) [div.box, div.box.this, div.box]
+
+//будет один эл так как два эл мы удалили
+console.log(boxesClass);//HTMLCollection [div.box] 
+
+//будет один эл, так как статичный массив
+console.log(Array.from(boxesClass));//[div.box] 
 
 for (let i =0; i<5; i++){
     const div = document.createElement('div');
     div.classList.add('box');
     document.body.append(div);
 }
-console.log(boxesSelector);//будет три эл
-console.log(boxesClass);//будет шесть эл 
-console.log(Array.from(boxesClass));//будет шесть эл 
+//будет три эл
+console.log(boxesSelector);//NodeList(3) [div.box, div.box.this, div.box]
+//будет шесть эл 
+console.log(boxesClass);//[div.box, div.box, div.box, div.box, div.box, div.box]
+//будет шесть эл 
+console.log(Array.from(boxesClass));//[div.box, div.box, div.box, div.box, div.box, div.box]
 
 
 
--- scripts/proverka.js
+++ scripts/proverka.js
@@ -1,4 +1,36 @@
 
-const block = document.querySelector('.block');
-console.log(block?.textContent);//undefind
-console.log(1+2);//3
+
+const boxesSelector = document.querySelectorAll('.box');
+const boxesClass = document.getElementsByClassName('box');
+
+
+console.log(boxesSelector);//[div.box, div#box.box]
+console.log(boxesClass);//[div.box, div#box.box, box: div#box.box]
+console.log(document.body.children);
+
+boxesSelector[0].remove();
+boxesClass[0].remove();
+
+console.log(boxesSelector);//будет три эл, так как это копия эл
+console.log(boxesClass);//будет один эл так как два эл мы удалили
+console.log(Array.from(boxesClass));//будет один эл, так как статичный массив
+console.log(Array.from(boxesSelector));
+
+for (let i =0; i<5; i++){
+    const div = document.createElement('div');
+    div.classList.add('box');
+    document.body.append(div);
+}
+console.log(boxesSelector);//будет три эл
+console.log(boxesClass);//будет шесть эл 
+console.log(Array.from(boxesClass));//будет шесть эл 
+console.log(Array.from(boxesSelector));//будет три эл 
+
+
+//метод matches для поиска среди эл массива элемента с еще одним классом
+boxesSelector.forEach(box =>{
+    if (box.matches('.this')) console.log(box);
+});
+
+//метод closest выводит первый эл с таким селектором среди родителей
+console.log(boxesSelector[0].closest('.wrapper'));
