4478cd36e5d21957b1956bacaf76351609fec551
Vladislav <vladozdeveloper@gmail.com>
2022-06-13 22:19:26 +0300

    completed lesson 74

--- scripts/lessons63-80/lesson74/lesson74.js
+++ scripts/lessons63-80/lesson74/lesson74.js
@@ -1,11 +1,16 @@
+// lesson 74 (MutationObserver, ResizeObserver и contenteditable)
 'use strict';
 
+//contenteditable позволет в блоке писать текст
+//<div contenteditable class="box"></div>
 const box = document.querySelector('.box');
 
-
+//сущность следит было ли изменение с элементом
 let observer = new MutationObserver(mutationRecord =>{
     console.log(mutationRecord);
 });
+
+//childList: true, если необходимо наблюдать за добавлением или удалением дочерних элементов (Включая текстовые узлы (text nodes))
 
 //метод obsorver следит за чем то
 //1 арг: выбираем элемент за которым следим
@@ -13,7 +18,7 @@ let observer = new MutationObserver(mutationRecord =>{
 observer.observe(box,{
     childList :true
 });
-
+//отключаем слежку за изменением
 observer.disconnect();
 
 //ResizeObserver отслеживает изменение размеров элементов
