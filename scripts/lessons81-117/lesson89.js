3eb6e7a22eec76dbfda39bd1e021b40583251bb1
Vladislav <vladozdeveloper@gmail.com>
2022-06-19 14:33:37 +0300

    writed command for npm and json

---
+++ scripts/lessons81-117/lesson89.js
@@ -0,0 +1,11 @@
+// lesson 89 (Npm и проект, JSON-server)
+"use strict";
+
+//инициализировать наш npm проект, вводим (npm init)
+//устанвливаем npm пакет json server локально, вводим (npm i json-server --save-dev)
+
+
+
+
+
+
