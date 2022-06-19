3da57de0003f2bbb9d8a88200cc0e9f6cfc04e97
Vladislav <vladozdeveloper@gmail.com>
2022-06-19 14:37:42 +0300

    created fetch request

--- scripts/lessons81-117/lesson89.js
+++ scripts/lessons81-117/lesson89.js
@@ -4,7 +4,11 @@
 //инициализировать наш npm проект, вводим (npm init)
 //устанвливаем npm пакет json server локально, вводим (npm i json-server --save-dev)
 
-
+//добавляем в проект food js файл
+//считываем данные, которые находятся в файде db.json
+fetch('db.json')
+    .then(data=>data.json())
+    .then(res=>console.log(res));
 
 
 
