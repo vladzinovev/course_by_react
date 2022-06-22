9bf394de0d1f7249e095576dfb41502cc5fb55e8
Vladislav <vladozdeveloper@gmail.com>
2022-06-22 20:52:56 +0300

    created show slides

--- scripts/lessons81-117/lesson92/js/lesson92.js
+++ scripts/lessons81-117/lesson92/js/lesson92.js
@@ -280,11 +280,35 @@ window.addEventListener('DOMContentLoaded', function() {
 
     let slideIndex = 1;
     const slides = document.querySelectorAll('.offer__slide'),
-        prev = document.querySelector('.offer__slider-prev'),
-        next = document.querySelector('.offer__slider-next'),
-        total = document.querySelector('#total'),
-        current = document.querySelector('#current');
+          prev = document.querySelector('.offer__slider-prev'),
+          next = document.querySelector('.offer__slider-next'),
+          total = document.querySelector('#total'),
+          current = document.querySelector('#current');
 
     
+
+
+    function showSlides(n) {
+        if (n > slides.length) {
+            slideIndex = 1;
+        }
+        if (n < 1) {
+            slideIndex = slides.length;
+        }
+
+        slides.forEach((item) => item.style.display = 'none');
+
+        slides[slideIndex - 1].style.display = 'block'; // Как ваша самостоятельная работа - переписать на использование классов show/hide
+        
+        if (slides.length < 10) {
+            current.textContent =  `0${slideIndex}`;
+        } else {
+            current.textContent =  slideIndex;
+        }
+    }
+
+    function plusSlides (n) {
+        showSlides(slideIndex += n);
+    }
 
 });
