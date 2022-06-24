fea2a33c276a7707f48aeffde2605c7c9f979573
Vladislav <vladozdeveloper@gmail.com>
2022-06-24 18:07:40 +0300

    added dots for slide pictures

--- scripts/lessons81-117/lesson94/js/lesson94.js
+++ scripts/lessons81-117/lesson94/js/lesson94.js
@@ -321,6 +321,31 @@ window.addEventListener('DOMContentLoaded', function() {
     `; // Если хотите - добавьте в стили, но иногда у нас нет доступа к стилям
     slider.append(indicators);
 
+    for (let i = 0; i < slides.length; i++) {
+        const dot = document.createElement('li');
+        dot.setAttribute('data-slide-to', i + 1);
+        dot.style.cssText = `
+            box-sizing: content-box;
+            flex: 0 1 auto;
+            width: 30px;
+            height: 6px;
+            margin-right: 3px;
+            margin-left: 3px;
+            cursor: pointer;
+            background-color: #fff;
+            background-clip: padding-box;
+            border-top: 10px solid transparent;
+            border-bottom: 10px solid transparent;
+            opacity: .5;
+            transition: opacity .6s ease;
+        `;
+        if (i == 0) {
+            dot.style.opacity = 1;
+        }
+        indicators.append(dot);
+        dots.push(dot);
+    }
+
     next.addEventListener('click', () => {
         if (offset == (+width.slice(0, width.length - 2) * (slides.length - 1))) {
             offset = 0;
