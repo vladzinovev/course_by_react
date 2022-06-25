8485c266cc0594f97de52a83f48061e1dc365893
Vladislav <vladozdeveloper@gmail.com>
2022-06-25 07:14:30 +0300

    work with local Storage

--- scripts/lessons81-117/lesson95.js
+++ scripts/lessons81-117/lesson95.js
@@ -1,2 +1,47 @@
 // lesson 95 (сохраняем данные без БД) 
 "use strict";
+
+//local Storage
+
+//записываем новый ключ (setitem)-1 арг -нзв ключа, 2арг - значение ключа
+localStorage.setitem('number',5);
+
+//получаем данные с local Storage
+localStorage.getitem('number');//5
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
