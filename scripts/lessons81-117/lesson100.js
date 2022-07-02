ae90f964f2359b75d18eef69440c4c01da27018b
Vladislav <vladozdeveloper@gmail.com>
2022-07-02 13:44:56 +0300

    added class with (set, set)

--- scripts/lessons81-117/lesson100.js
+++ scripts/lessons81-117/lesson100.js
@@ -33,7 +33,7 @@ ivan.userAge=30;
 ivan.say();//Имя пользователя: Alex, возраст 27
 
 */
-
+//constructor
 function User(name, age){
     this.name=name;
     let userAge=age;
@@ -60,3 +60,68 @@ ivan.setAge(30);
 ivan.say();//Имя пользователя: Alex, возраст 30
 
 
+//class
+class User {
+    constructor(name, age) {
+        this.name = name;
+        this.userAge=age;
+    }
+
+
+    say(){
+        console.log(`Имя пользователя: ${this.name}, возраст ${this.userAge}`);
+    }
+
+    getAge() {
+        return this.userAge;
+    }
+
+    setAge(age) {
+        if (typeof age === 'number' && age > 0 && age < 110) {
+            this.userAge = age;
+        } else {
+            console.log('Недопустимое значение!');
+        }
+    }
+}
+const ivan = new User('Ivan', 27);
+console.log(ivan.name);//Ivan
+ivan.userAge=99;
+console.log(ivan.getAge());//99
+
+ivan.name='Alex';
+ivan.say();//Имя пользователя: Alex, возраст 99
+
+
+
+
+
+
+
+class User {
+    constructor(name, age) {
+        this.name = name;
+        this._age = age;
+    }
+
+    #surname = 'Petrychenko';
+
+    say=() => {
+        console.log(`Имя пользователя: ${this.name} ${this.#surname}, возраст ${this._age}`);
+    }
+
+    get age() {
+        return this._age;
+    }
+
+    set age(age) {
+        if (typeof age === 'number' && age > 0 && age < 110) {
+            this._age = age;
+        } else {
+            console.log('Недопустимое значение!');
+        }
+    }
+}
+const ivan = new User('Ivan', 27);
+console.log(ivan.surname);//undefind
+ivan.say();//Имя пользователя: Ivan Petrychenko, возраст 27
