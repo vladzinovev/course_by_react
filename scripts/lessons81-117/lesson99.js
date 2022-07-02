d072d1125ab1e6b38ba24488f573f228931c989f
Vladislav <vladozdeveloper@gmail.com>
2022-07-02 13:15:51 +0300

    added get for persone

---
+++ scripts/lessons81-117/lesson99.js
@@ -0,0 +1,19 @@
+// lesson 99 (get/set) 
+"use strict";
+
+
+const persone ={
+    name:"Alex",
+    age:25,
+
+    get userAge(){
+        return this.age;
+    }
+};
+console.log(persone.userAge);//25
+
+
+
+
+
+
