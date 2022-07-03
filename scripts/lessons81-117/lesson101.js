d7ffe3e8c4dd4383fff0a6af0de6a8c8f67da2f9
Vladislav <vladozdeveloper@gmail.com>
2022-07-03 15:41:23 +0300

    completed lesson 101

--- scripts/lessons81-117/lesson101.js
+++ scripts/lessons81-117/lesson101.js
@@ -6,12 +6,14 @@ const number = 1;
 //анонимная самовызывающаяся функция
 (function(){
     let numbet =2;
+    //видит number только внутри функции
     console.log(number);//2
     console.log(number + 3);//5
 }());
-console.log(number);//1
+//number видит, который за функцией
+console.log(number);//1 
 
-//обьектный интерфейс
+//обьектный интерфейс с самовызывающейся функцией
 const user = (function(){
     const privat = function(){
         console.log('I am privat!');
