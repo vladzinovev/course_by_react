121d2d76c24985d00824d8ea254b11c3842a19f0
Vladislav <vladozdeveloper@gmail.com>
2022-07-04 17:28:13 +0300

    created js for import our function

---
+++ scripts/lessons81-117/lesson102/main.js
@@ -0,0 +1,11 @@
+
+//импортируем нашу функицю из lesson102
+const myModule=require('./lesson102');
+
+const myModuleInstance=new myModule();
+myModuleInstance.hello();
+myModuleInstance.goodbye();
+
+
+
+
