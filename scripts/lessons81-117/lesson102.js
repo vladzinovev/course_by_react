38e707bcdd0dbd0a164ff70e7df3eae785add159
Vladislav <vladozdeveloper@gmail.com>
2022-07-04 17:24:47 +0300

    created folder and new function in the js for exports

--- scripts/lessons81-117/lesson102.js
+++
@@ -1,2 +0,0 @@
-// lesson 102 (webpack) 
-'use strict';
---
+++ scripts/lessons81-117/lesson102/lesson102.js
@@ -0,0 +1,29 @@
+// lesson 102 (webpack) 
+'use strict';
+
+
+
+function myModule() {
+    this.hello = function(){
+        console.log('hello');
+    };
+    this.goodbye=function(){
+        console.log('bye!');
+    };
+}
+
+//синтаксис commandjs для нашей функции в общий js файл
+module.exports=myModule;
+
+
+
+
+
+
+
+
+
+
+
+
+
