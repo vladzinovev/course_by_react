ea3bef7cbb7f77fdaa44afb73552d44cd04c45a7
Vladislav <vladozdeveloper@gmail.com>
2022-07-06 23:38:21 +0300

    fixed bugs selectors initialization

--- scripts/lessons81-117/lesson104/js/script.js
+++ scripts/lessons81-117/lesson104/js/script.js
@@ -10,12 +10,20 @@ import {openModal} from './modules/modal';
 window.addEventListener('DOMContentLoaded', function() {
     const modalTimerId = setTimeout(() => openModal('.modal', modalTimerId), 50000);
 
-
-    tabs();
+    tabs('.tabheader__item', '.tabcontent', '.tabheader__items', 'tabheader__item_active');
     modal('[data-modal]', '.modal', modalTimerId);
-    timer();
+    timer('.timer', '2022-06-11');
     cards();
     calc();
-    forms();
-    slider();
+    forms('form', modalTimerId);
+    slider({
+        container: '.offer__slider',
+        slide: '.offer__slide',
+        nextArrow: '.offer__slider-next',
+        prevArrow: '.offer__slider-prev',
+        totalCounter: '#total',
+        currentCounter: '#current',
+        wrapper: '.offer__slider-wrapper',
+        field: '.offer__slider-inner'
+    });
 });
