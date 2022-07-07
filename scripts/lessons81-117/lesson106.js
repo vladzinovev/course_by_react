e889163fb16457290156603ca7f5dff8b3205439
Vladislav <vladozdeveloper@gmail.com>
2022-07-07 22:29:44 +0300

    completed lesson 106

---
+++ scripts/lessons81-117/lesson106.js
@@ -0,0 +1,32 @@
+// lesson 101 (ошибки) 
+'use strict';
+
+try{
+    console.log('Normal');
+    console.log(a);
+    console.log('result');
+} catch(e){
+    console.log('error');
+    console.log(e);
+}
+
+console.log('Still normal');
+//Normal   
+//error  
+//ReferenceError: a is not defined  
+//Still normal
+
+
+
+try{
+    console.log(a);
+} catch(e){
+    console.log(e.name);
+    console.log(e.message);
+    console.log(e.stack);
+}finally{
+    
+}
+//ReferenceError
+//a is not defined
+//ReferenceError: a is not defined
