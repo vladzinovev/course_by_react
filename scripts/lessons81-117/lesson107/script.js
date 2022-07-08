19b0dd8911bd4319693f95bbf067794dd18bc871
Vladislav <vladozdeveloper@gmail.com>
2022-07-08 19:04:47 +0300

    fixed data for error

--- scripts/lessons81-117/lesson107/script.js
+++ scripts/lessons81-117/lesson107/script.js
@@ -13,10 +13,13 @@ const data = [
         id: 'circle',
         tag: 'span'
     }
-]
+];
 
-data.forEach(blockObj => {
+data.forEach((blockObj, i) => {
     const block = document.createElement(blockObj.tag);
+    
+    if (!blockObj.id) throw new Error(`В данных под номером ${i + 1} нет id`);
+
     block.setAttribute('id', blockObj.id);
     document.body.append(block);
-})
+});
