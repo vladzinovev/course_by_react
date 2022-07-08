99bcd035046d5eb20e01d76421b3bac94ba274be
Vladislav <vladozdeveloper@gmail.com>
2022-07-08 19:21:22 +0300

    added try/catch

--- scripts/lessons81-117/lesson107/script.js
+++ scripts/lessons81-117/lesson107/script.js
@@ -15,6 +15,28 @@ const data = [
     }
 ];
 
+try{
+    data.forEach((blockObj, i) => {
+        const block = document.createElement(blockObj.tag);
+        
+        if (!blockObj.id) throw new SyntaxError(`В данных под номером ${i + 1} нет id`);
+    
+        block.setAttribute('id', blockObj.id);
+        document.body.append(block);
+    });
+} catch(e){
+    
+    console.log(e.name);
+    console.log(e.message);
+    console.log(e.stack);
+    //будет выделено красным
+    console.error(e.name);
+    console.error(e.message);
+    console.error(e.stack); 
+    
+
+}
+
 data.forEach((blockObj, i) => {
     const block = document.createElement(blockObj.tag);
     
@@ -26,3 +48,11 @@ data.forEach((blockObj, i) => {
 
 const err = new Error('eerrrroorr');
 console.log(err.name, err.message, err.stack);//Error, eerrrroorr, Error: eerrrroorr
+
+
+const er = new SyntaxError('eerrrroorr');
+console.log(er.name, er.message, er.stack);//SyntaxError, eerrrroorr, SyntaxError: eerrrroorr
+
+
+
+
