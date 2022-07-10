7528a6fba2c1fd141c69254536fff11fcd529144
Vladislav <vladozdeveloper@gmail.com>
2022-07-10 23:38:53 +0300

    finished working with variables in jquerry and completed lesson110

---
+++ scripts/lessons81-117/lesson110/script.js
@@ -0,0 +1,28 @@
+import 'jquery';
+import $ from 'jquery'; 
+
+
+
+const btn = $('#btn');
+console.log(btn);
+
+
+
+$(document).ready(function(){
+    /* при навдедние на кнопку а у нее класс сатновится активным */
+    $('.list-item:first').hover(function(){
+        $(this).toggleClass('active');
+    });
+    /* при нажатии на третью кнопку четная картинка исчезает плавно */
+    $('.list-item:eq(2)').on('click',function(){
+        $('.image:even').fadeToggle('slow');
+    });
+    /*  при нажатии на четвертую кнопку нечетная картинка плавно уменьшается до исчезновения */
+    $('.list-item:eq(4)').on('click',function(){
+        $('.image:odd').animate({
+            opacity:'toogle',
+            height:'toogle'
+        },2000);
+    });
+
+});
