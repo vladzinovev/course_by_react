2124f40b0a4056d2551d80792389832e799aca06
Vladislav <vladozdeveloper@gmail.com>
2022-07-11 17:43:09 +0300

    added function generator with number and completed lesson 111

--- scripts/lessons81-117/lesson111.js
+++ scripts/lessons81-117/lesson111.js
@@ -13,11 +13,28 @@ function* generator(){
 
 
 const str= generator();
-console.log(str.next());//{ value: 'S', done: false }
+console.log(str.next().value);// S
 console.log(str.next().value);// c
 console.log(str.next());//{ value: 'r', done: false }
 console.log(str.next());//{ value: 'i', done: false }
 console.log(str.next());//{ value: 'p', done: false }
 console.log(str.next());//{ value: 't', done: false }
 console.log(str.next());//{ value: undefined, done: true }
+
+
+
+function* count(n){
+    for(let i=0;i<n;i++){
+       yield i; 
+    }
+}
+const counter=count(7);
+
+console.log(counter.next().value);//0
+console.log(counter.next().value);//1
+console.log(counter.next().value);//2
+//или
+for (let k of count(7)){
+    console.log(k); // 0 1 2 3 4 5 6 7 
+}
 
