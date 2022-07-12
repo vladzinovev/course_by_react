f43ac28b96e42e07ecb0dc77ed1eb07dcce4027d
Vladislav <vladozdeveloper@gmail.com>
2022-07-12 23:18:08 +0300

    added materials for lesson 113

--- scripts/lessons81-117/lesson113/script.js
+++ scripts/lessons81-117/lesson113/script.js
@@ -1,16 +1,3 @@
 // lesson 113 (Web Animation API) 
 'use strict';
 
-const btnPhone = document.querySelector('#iphone'),
-      btnMacbook = document.querySelector('#macbook'),
-      images = document.querySelectorAll('img');
-
-const phoneAnimation = images[0].animate([
-    {transform:'translateY(0)'},
-    {transform:'translateY(100px)'},
-    {transform:'translateY(-100px)'},
-    {transform:'translateY(0)'},
-], {
-    duration:3000,
-    iteration:Infinity
-});
