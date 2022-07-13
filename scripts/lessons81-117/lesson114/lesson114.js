c5cc1627bfe459852593beec82642a979910e2b7
Vladislav <vladozdeveloper@gmail.com>
2022-07-13 17:14:30 +0300

    created Example 3 asynchronous function

--- scripts/lessons81-117/lesson114/lesson114.js
+++ scripts/lessons81-117/lesson114/lesson114.js
@@ -1,13 +1,15 @@
 // lesson 114 (Event loop) 
 'use strict';
 
+
+//Пример 1
+
 /* 
 console.log(1);
 
 setTimeout(()=>{
     console.log(2000);
 },2000);
-
 
 setTimeout(()=>{
     console.log(4000);
@@ -20,8 +22,8 @@ console.log(2);
 //2000
 //4000
 
+//Пример 2
 //мы не можем ничего сделать на странице, пока не выведится alert
-
 let k=0;
 function count(){
     for (let i=0;i<1000000000;i++){
@@ -32,8 +34,15 @@ function count(){
 count();
 
 
+//Пример 3
+setTimeout(()=>{
+    console.log(1);
+},0);
 
+console.log(2);
 
+//2
+//1
 
 
 
