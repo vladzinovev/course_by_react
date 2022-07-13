73e061fa77a978dc4e138197369ec9a9fad0e0de
Vladislav <vladozdeveloper@gmail.com>
2022-07-13 17:10:11 +0300

    created function (sequence of actions)

--- scripts/lessons81-117/lesson114/lesson114.js
+++ scripts/lessons81-117/lesson114/lesson114.js
@@ -1,7 +1,7 @@
 // lesson 114 (Event loop) 
 'use strict';
 
-
+/* 
 console.log(1);
 
 setTimeout(()=>{
@@ -14,13 +14,22 @@ setTimeout(()=>{
 },4000);
 
 console.log(2);
-
+ */
 //1
 //2
 //2000
 //4000
 
+//мы не можем ничего сделать на странице, пока не выведится alert
 
+let k=0;
+function count(){
+    for (let i=0;i<1000000000;i++){
+        k++;
+    }
+    alert('done');
+}
+count();
 
 
 
