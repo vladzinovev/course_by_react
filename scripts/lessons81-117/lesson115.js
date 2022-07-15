790414f50979b29b5ccfb28a154efdfd9c246095
Vladislav <vladozdeveloper@gmail.com>
2022-07-15 22:01:20 +0300

    added two examples

--- scripts/lessons81-117/lesson115.js
+++ scripts/lessons81-117/lesson115.js
@@ -24,3 +24,43 @@ console.log('code');
 //()=>{}
 
 
+setTimeout(()=>console.log('timeout'));
+setTimeout(()=>console.log('timeout2'));
+Promise.resolve()
+    .then(()=>console.log('promise'));
+
+console.log('code');
+
+Promise.resolve()
+    .then(()=>console.log('promise2'));
+
+console.log('code2');
+/* 
+code
+code2
+promise
+promise2
+timeout
+timeout2
+*/
+
+
+
+setTimeout(()=>console.log('timeout'));
+
+Promise.resolve()
+    .then(()=>console.log('promise'));
+
+queueMicrotask(()=>console.log('wow'));
+
+Promise.resolve()
+    .then(()=>console.log('promise2'));
+
+console.log('code');
+/* 
+code
+promise
+wow
+promise2
+timeout
+*/
