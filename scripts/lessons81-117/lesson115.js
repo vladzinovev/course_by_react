d301934f4afdc12a28a7114344342bfaaeed1259
Vladislav <vladozdeveloper@gmail.com>
2022-07-15 21:36:30 +0300

    sequence of actions

---
+++ scripts/lessons81-117/lesson115.js
@@ -0,0 +1,20 @@
+// lesson 115 (макро и микрозадачи)
+"use strict";
+
+setTimeout(()=>console.log('timeout'));
+
+Promise.resolve()
+    .then(()=>console.log('promise'));
+
+console.log('code');
+
+
+//code
+//promise
+//timeout
+
+
+
+
+
+
