b53cb0bc5715619998fdbc67306a13193aa40065
Vladislav <vladozdeveloper@gmail.com>
2022-07-18 23:36:19 +0300

    tested react app

--- scripts/lessons118-146/lesson121/my-app/src/App.js
+++ scripts/lessons118-146/lesson121/my-app/src/App.js
@@ -9,7 +9,10 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        
+        <h1>Hello world!</h1>
+        <h2>This is my first react app.</h2>
+        <h3>Welcome to my app.</h3>
+        <h4>My name is Vlad.</h4>
       </header>
     </div>
   );
