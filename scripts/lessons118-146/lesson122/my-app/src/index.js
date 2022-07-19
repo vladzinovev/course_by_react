8013eb621cef034abddfddd74d049e8ef803a85c
Vladislav <vladozdeveloper@gmail.com>
2022-07-19 20:16:39 +0300

    created alem with react and added elem structure

--- scripts/lessons118-146/lesson122/my-app/src/index.js
+++ scripts/lessons118-146/lesson122/my-app/src/index.js
@@ -3,11 +3,23 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-const elem = <h2>Hello world!</h2>;
+//const elem = <h2>Hello world!</h2>;
+const elem = React.createElement('h2',null,'Hello world!');
+const elem2 = React.createElement('h2',{className:'greetings'},'Hello world2!');
+
+const element = {
+  type:'h2',
+  props:{
+    className:'greeting',
+    children:'Hello world!'
+  }
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  elem
+  elem,
+  elem2,
+  element
 );
 
 
