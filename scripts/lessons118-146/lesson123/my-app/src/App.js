c543c9e1f3518b22d195928b48285c5856fd156b
Vladislav <vladozdeveloper@gmail.com>
2022-07-20 23:41:25 +0300

    completed our components

--- scripts/lessons118-146/lesson123/my-app/src/App.js
+++ scripts/lessons118-146/lesson123/my-app/src/App.js
@@ -6,10 +6,30 @@ const Header =()=>{
   return <h2>Hello world!</h2>
 }
 
+const Field=()=>{
+  return <input placeholder="Type here" typ="text"/>
+}
+function Btn(){
+  const text ='Log in';
+  return <button>{text}</button>
+
+  //const p =<p>Log in</p>
+  //return <button>{p}</button>
+
+  /* 
+  const res = ()=>{
+    return 'Log in'
+  }
+  return <button>{res()}</button> 
+  */
+}
+
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Field/>
+      <Btn/>
     </div>
   );
 }
