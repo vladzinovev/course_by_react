ec391f72b0dea1cab3df65e9282d5153ff7df0fd
Vladislav <vladozdeveloper@gmail.com>
2022-07-20 23:32:33 +0300

    created react component

--- scripts/lessons118-146/lesson123/my-app/src/App.js
+++ scripts/lessons118-146/lesson123/my-app/src/App.js
@@ -1,16 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 
+//создали react компонент
+const Header =()=>{
+  return <h2>Hello world!</h2>
+}
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        
-      </header>
+      <Header/>
     </div>
   );
 }
