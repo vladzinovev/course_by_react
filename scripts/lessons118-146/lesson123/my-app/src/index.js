a31a351e65c119ac1f976ad6089f88be0782b35d
Vladislav <vladozdeveloper@gmail.com>
2022-07-20 23:08:22 +0300

    added files for lesson123

---
+++ scripts/lessons118-146/lesson123/my-app/src/index.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+//const elem = <h2>Hello world!</h2>;
+const elem = React.createElement('h2',null,'Hello world!');
+const elem2 = React.createElement('h2',{className:'greetings'},'Hello world2!');
+
+const element = {
+  type:'h2',
+  props:{
+    className:'greeting',
+    children:'Hello world!'
+  }
+};
+
+const text="Hello world";
+const elem3 =(
+  <div>
+    <h2 className='text'>Hello world!</h2>
+    <h3>{text}</h3>
+    <input type="text" />
+    <label htmlFor=''></label>
+    <button>Click</button>
+    <button/>
+  </div>
+);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  elem,
+  elem2,
+  element
+);
+
+
