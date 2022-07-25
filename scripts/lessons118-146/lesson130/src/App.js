002d04925c78f8c9be38072a860a91e8da89f205
Vladislav <vladozdeveloper@gmail.com>
2022-07-25 21:38:14 +0300

    created class

---
+++ scripts/lessons118-146/lesson130/src/App.js
@@ -0,0 +1,44 @@
+import { Component } from 'react';
+import './App.css';
+/* 
+function WhoAmI(props){
+  return (
+    <div>
+      <h1>My name is {props.name}, surname - {props.surname}</h1>
+      <a href={props.link}>My profile</a>
+    </div>
+  )
+}
+*/
+/* 
+function WhoAmI({name,surname,link}){
+  return (
+    <div>
+      <h1>My name is {name.firstName}, surname - {surname()}</h1>
+      <a href={link}>My profile</a>
+    </div>
+  )
+}
+*/
+
+class WhoAmI extends Component{
+  render(){
+    return (
+      <div>
+        <h1>My name is {name.firstName}, surname - {surname()}</h1>
+        <a href={link}>My profile</a>
+      </div>
+    )
+  }
+}
+
+function App() {
+  return (
+    <div className="App">
+      <WhoAmI name={{firstName:'John'}} surname={()=>{return 'Smith'}} link="facebook.com"/>
+      <WhoAmI name={{firstName:'Alex'}} surname={()=>{return 'Smith'}} link="vk.com"/>
+    </div>
+  );
+}
+
+export default App;
