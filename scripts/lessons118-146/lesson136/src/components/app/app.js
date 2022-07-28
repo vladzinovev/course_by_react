341aca147d87c19363f19c63bafc47d0ad65ea36
Vladislav <vladozdeveloper@gmail.com>
2022-07-28 22:40:48 +0300

    added onToggleProp in app

--- scripts/lessons118-146/lesson136/src/components/app/app.js
+++ scripts/lessons118-146/lesson136/src/components/app/app.js
@@ -45,10 +45,23 @@ class App extends Component {
         });
     }
 
+    onToggleProp = (id, prop) => {
+        this.setState(({data}) => ({
+            data: data.map(item => {
+                if (item.id === id) {
+                    return {...item, [prop]: !item[prop]}
+                }
+                return item;
+            })
+        }))
+    }
+
     render() {
+        const employees = this.state.data.length;
+        const increased = this.state.data.filter(item => item.increase).length;
         return (
             <div className="app">
-                <AppInfo />
+                <AppInfo employees={employees} increased={increased}/>
     
                 <div className="search-panel">
                     <SearchPanel/>
@@ -57,7 +70,8 @@ class App extends Component {
                 
                 <EmployeesList 
                     data={this.state.data}
-                    onDelete={this.deleteItem}/>
+                    onDelete={this.deleteItem}
+                    onToggleProp={this.onToggleProp}/>
                 <EmployeesAddForm onAdd={this.addItem}/>
             </div>
         );
