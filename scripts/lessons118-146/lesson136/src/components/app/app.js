3ff949e84d923b6d284b0f9120bf555bcd019b12
Vladislav <vladozdeveloper@gmail.com>
2022-07-28 22:29:27 +0300

    added materials for lesson

--- scripts/lessons118-146/lesson136/src/components/app/app.js
+++ scripts/lessons118-146/lesson136/src/components/app/app.js
@@ -28,6 +28,23 @@ class App extends Component {
             }
         })
     }
+
+    // Да, пока могут добавляться пустые пользователи. Мы это еще исправим
+    addItem = (name, salary) => {
+        const newItem = {
+            name, 
+            salary,
+            increase: false,
+            id: this.maxId++
+        }
+        this.setState(({data}) => {
+            const newArr = [...data, newItem];
+            return {
+                data: newArr
+            }
+        });
+    }
+
     render() {
         return (
             <div className="app">
@@ -41,7 +58,7 @@ class App extends Component {
                 <EmployeesList 
                     data={this.state.data}
                     onDelete={this.deleteItem}/>
-                <EmployeesAddForm />
+                <EmployeesAddForm onAdd={this.addItem}/>
             </div>
         );
     }
