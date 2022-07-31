5682af3254b8ca97cf488650f250c5e438fe80a3
Vladislav <vladozdeveloper@gmail.com>
2022-07-31 15:21:35 +0300

    added button for filter and classes

--- scripts/lessons118-146/lesson138/src/components/app-filter/app-filter.js
+++ scripts/lessons118-146/lesson138/src/components/app-filter/app-filter.js
@@ -1,8 +1,29 @@
 import "./app-filter.css";
 
-const AppFilter = () => {
+const AppFilter = (props) => {
+    const buttonsData = [
+        {name: 'all', label: 'Все сотрудники'},
+        {name: 'rise', label: 'На повышение'},
+        {name: 'moreThen1000', label: 'З/П больше 1000$'}
+    ];
+
+    const buttons = buttonsData.map(({name, label}) => {
+        const active = props.filter === name;
+        const clazz = active ? 'btn-light' : 'btn-outline-light';
+        return (
+            <button type="button"
+                    className={`btn ${clazz}`}
+                    key={name}
+                    onClick={() => props.onFilterSelect(name)}>
+                    {label}
+            </button>
+        )
+    })
+
     return (
         <div className="btn-group">
+            {buttons}
+{/* 
             <button type="button"
                     className="btn btn-light">
                     Все сотрудники
@@ -15,6 +36,7 @@ const AppFilter = () => {
                     className="btn btn-outline-light">
                     З/П больше 1000$
             </button>
+             */}
         </div>
     )
 }
