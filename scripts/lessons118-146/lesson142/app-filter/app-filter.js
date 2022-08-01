52d3722d276f73dd9849ddb6605c970945ec8e48
Vladislav <vladozdeveloper@gmail.com>
2022-08-01 20:40:08 +0300

    added files for lesson142

---
+++ scripts/lessons118-146/lesson142/app-filter/app-filter.js
@@ -0,0 +1,44 @@
+import "./app-filter.css";
+
+const AppFilter = (props) => {
+    const buttonsData = [
+        {name: 'all', label: 'Все сотрудники'},
+        {name: 'rise', label: 'На повышение'},
+        {name: 'moreThen1000', label: 'З/П больше 1000$'}
+    ];
+
+    const buttons = buttonsData.map(({name, label}) => {
+        const active = props.filter === name;
+        const clazz = active ? 'btn-light' : 'btn-outline-light';
+        return (
+            <button type="button"
+                    className={`btn ${clazz}`}
+                    key={name}
+                    onClick={() => props.onFilterSelect(name)}>
+                    {label}
+            </button>
+        )
+    })
+
+    return (
+        <div className="btn-group">
+            {buttons}
+{/* 
+            <button type="button"
+                    className="btn btn-light">
+                    Все сотрудники
+            </button>
+            <button type="button"
+                    className="btn btn-outline-light">
+                    На повышение
+            </button>
+            <button type="button"
+                    className="btn btn-outline-light">
+                    З/П больше 1000$
+            </button>
+             */}
+        </div>
+    )
+}
+
+export default AppFilter;
