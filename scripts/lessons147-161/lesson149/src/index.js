ef1999b9625089c9901b712c71800ebfba316e47
Vladislav <vladozdeveloper@gmail.com>
2022-08-05 21:50:15 +0300

    uploaded data and displayed on the page

--- scripts/lessons147-161/lesson149/src/index.js
+++ scripts/lessons147-161/lesson149/src/index.js
@@ -1,13 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/App';
-import MarvelService from './services/MarvelService';
 import './style/style.scss';
-
-//создали экземпляр класса
-const marvelService = new MarvelService();
-//выводим имена всех персонажей
-marvelService.getAllCharacters().then(res => res.data.results.forEach(item => console.log(item.name)));
 
 ReactDOM.render(
   <React.StrictMode>
