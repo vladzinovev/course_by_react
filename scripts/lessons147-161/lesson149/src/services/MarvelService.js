e6acacf646e06c2dc5d200e63b1ec3659b70b5f3
Vladislav <vladozdeveloper@gmail.com>
2022-08-05 22:02:36 +0300

    optimized the code

--- scripts/lessons147-161/lesson149/src/services/MarvelService.js
+++ scripts/lessons147-161/lesson149/src/services/MarvelService.js
@@ -19,16 +19,16 @@ class MarvelService {
     //получение одного персонажа
     getCharacter = async (id) => {
         const res = await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
-        return this._transformCharacter(res);
+        return this._transformCharacter(res.data.results[0]);
     }
 
-    _transformCharacter = (res) => {
+    _transformCharacter = (char) => {
         return {
-            name: res.name,
-            description: res.description ? `${char.description.slice(0, 210)}...` : 'There is no description for this character',
-            thumbnail: res.thumbnail.path + '.' + char.thumbnail.extension,
-            homepage: res.urls[0].url,
-            wiki: res.urls[1].url
+            name: char.name,
+            description: char.description ? `${char.description.slice(0, 210)}...` : 'There is no description for this character',
+            thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
+            homepage: char.urls[0].url,
+            wiki: char.urls[1].url
         }
     }
 }
