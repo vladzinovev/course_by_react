2fd121a06c3620287fd1d0b5c8a73bf1d5a9481d
Vladislav <vladozdeveloper@gmail.com>
2022-08-16 23:41:48 +0300

    completed lesson with ref

--- scripts/lessons147-161/lesson159/App.js
+++ scripts/lessons147-161/lesson159/App.js
@@ -5,6 +5,15 @@ import './App.css';
 class Form extends Component {
     constructor(props) {
         super(props);
+        this.myRef = React.createRef();
+        this.MyNewRef= React.createRef();
+    }
+    //при открытии страницы, фокус мышки сразу попадает в строку ввода mail 
+    componentDidMount(){
+        //1 способ
+        this.myRef.current.focus();
+        //2 способ
+        this.MyNewRef.current.doSmth();
     }
 
     render() {
@@ -13,7 +22,10 @@ class Form extends Component {
                 <form className="w-50 border mt-5 p-3 m-auto">
                     <div className="mb-3">
                         <label htmlFor="exampleFormControlInput1" className="form-label">Email address</label>
-                        <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
+                        //1 способ
+                        <input ref={this.myRef} type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
+                        //2 способ
+                        <TextInput ref={this.MyNewRef}/>
                     </div>
                     <div className="mb-3">
                         <label htmlFor="exampleFormControlTextarea1" className="form-label">Example textarea</label>
@@ -21,6 +33,21 @@ class Form extends Component {
                     </div>
                 </form>
             </Container>
+        )
+    }
+}
+class TextInput extends Component{
+    doSmth=()=>{
+        console.log('smth');
+    }
+
+    render(){
+        return(
+            <input 
+            type="email"
+            className="form-control"
+            id="exampleFormControlInput1"
+            placeholder="name@example.com"/>
         )
     }
 }
