1ddb9ea459a5b486d59ed13a5ae37db44d38c047
Vladislav <vladozdeveloper@gmail.com>
2022-08-16 23:15:57 +0300

    added app.js

---
+++ scripts/lessons147-161/lesson159/App.js
@@ -0,0 +1,34 @@
+import React, {Component} from 'react';
+import {Container} from 'react-bootstrap';
+import './App.css';
+
+class Form extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <Container>
+                <form className="w-50 border mt-5 p-3 m-auto">
+                    <div className="mb-3">
+                        <label htmlFor="exampleFormControlInput1" className="form-label">Email address</label>
+                        <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="exampleFormControlTextarea1" className="form-label">Example textarea</label>
+                        <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                    </div>
+                </form>
+            </Container>
+        )
+    }
+}
+
+function App() {
+    return (
+        <Form/>
+    );
+}
+
+export default App;
