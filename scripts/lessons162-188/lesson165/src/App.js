8a395937cd51f249311c4950a3db275d45de08ff
Vladislav <vladozdeveloper@gmail.com>
2022-08-21 23:15:21 +0300

    created useCallback on the function getSomeImages

--- scripts/lessons162-188/lesson165/src/App.js
+++ scripts/lessons162-188/lesson165/src/App.js
@@ -1,79 +1,15 @@
-import {Component, useState, useEffect} from 'react';
+import {useState, useCallback,useEffect} from 'react';
 import {Container} from 'react-bootstrap';
 import './App.css';
 
-/* 
-class Slider extends Component {
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            autoplay: false,
-            slide: 0
-        }
-    }
-
-    componentDidMount(){
-        //не меняется значение тайтла
-        document.title=`Slide:${this.state.slide}`;
-    }
-    componentDidUpdate(){
-        //меняется значение тайтла
-        document.title=`Slide:${this.state.slide}`;
-    }
-
-    changeSlide = (i) => {
-        this.setState(({slide}) => ({
-            slide: slide + i
-        }))
-    }
-
-    toggleAutoplay = () => {
-        this.setState(({autoplay}) => ({
-            autoplay: !autoplay
-        }))
-    }
-
-    render() {
-        return (
-            <Container>
-                <div className="slider w-50 m-auto">
-                    <img className="d-block w-100" src="https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg" alt="slide" />
-                    <div className="text-center mt-5">Active slide {this.state.slide} <br/> {this.state.autoplay ? 'auto' : null}</div>
-                    <div className="buttons mt-3">
-                        <button 
-                            className="btn btn-primary me-2"
-                            onClick={() => this.changeSlide(-1)}>-1</button>
-                        <button 
-                            className="btn btn-primary me-2"
-                            onClick={() => this.changeSlide(1)}>+1</button>
-                        <button 
-                            className="btn btn-primary me-2"
-                            onClick={this.toggleAutoplay}>toggle autoplay</button>
-                    </div>
-                </div>
-            </Container>
-        )
-    }
-} 
- */
-
-
-const calcValue=()=>{
-    console.log('random');
-    return Math.random()*(50-1)+1;
-}
-
-const getSomeImages=()=>{
-    console.log('fetching')
-    return[
-        "https://ru.reactjs.org/logo-og.png",
-        "https://media.tproger.ru/uploads/2016/10/reactmini.png"
-    ]
-}
-
 const Slider = (props) => {
-
+    const getSomeImages=useCallback(()=>{
+        console.log('fetching')
+        return[
+            "https://ru.reactjs.org/logo-og.png",
+            "https://media.tproger.ru/uploads/2016/10/reactmini.png"
+        ]
+    },[]);
 
 
     //старый вариант
@@ -94,7 +30,7 @@ const Slider = (props) => {
     return (
         <Container>
             <div className="slider w-50 m-auto">
-
+                /* 
                 {
                     getSomeImages().map((url,i)=>{
                         return(
@@ -102,6 +38,9 @@ const Slider = (props) => {
                         )
                     })
                 }
+                */
+
+                <Slide getSomeImages={getSomeImages}/>
 
                 <div className="text-center mt-5">Active slide {slide} <br/> {autoplay ? 'auto' : null}</div>
                 <div className="buttons mt-3">
@@ -119,16 +58,23 @@ const Slider = (props) => {
         </Container>
     )
 }
+const Slide=({getSomeImages})=>{
+    const[images,setImages]=useState([]);
+    useEffect(()=>{
+        setImages(getSomeImages())
+    },[getSomeImages])
+
+    return (
+        <>
+            {images.map((url,i)=><img key={i} className="d-block w-100" src={url} alt="slide"/>)}
+        </>
+    )
+}
 
 
 function App() {
-    const [slider,setSlider]=useState(true);
     return (
-        <>
-        <button onClick={()=>setSlider(false)}>Click</button>
-            {slider ? <Slider/> : null}
-        </>
-        
+        <Slider/>
     );
 }
 
