8cb7a8ffd39b17cad31a02108ddcf27481e25fc5
Vladislav <vladozdeveloper@gmail.com>
2022-08-22 23:20:18 +0300

    added style with useEffect on the countTotal

--- scripts/lessons162-188/lesson166/src/App.js
+++ scripts/lessons162-188/lesson166/src/App.js
@@ -36,6 +36,14 @@ const Slider = (props) => {
         return countTotal(slide);
     }, [slide]);
 
+    const style=useMemo(()=>({
+        color:slide>4 ?'red' :'black'
+    }),[slide])
+
+    useEffect(()=>{
+        console.log('styled!')
+    },[style]);
+
     return (
         <Container>
             <div className="slider w-50 m-auto">
@@ -44,7 +52,7 @@ const Slider = (props) => {
                 <Slide getSomeImages={getSomeImages}/>
 
                 <div className="text-center mt-5">Active slide {slide} <br/> {autoplay ? 'auto' : null}</div>
-                <div className="text-center mt-5">Total slides: {total}</div>
+                <div style={style} className="text-center mt-5">Total slides: {total}</div>
                 <div className="buttons mt-3">
                     <button 
                         className="btn btn-primary me-2"
