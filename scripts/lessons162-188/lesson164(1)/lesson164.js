df1295b88ee7894ec3a195d1063e3056f47bc6a0
Vladislav <vladozdeveloper@gmail.com>
2022-08-23 17:08:32 +0300

    fixed bugs in the home work

--- scripts/lessons162-188/lesson164(1)/lesson164.js
+++ scripts/lessons162-188/lesson164(1)/lesson164.js
@@ -1,47 +1,50 @@
-const {useState, useEffect} = React;
+
 
 const App = (props) => {
-  const [userValue, setUserValue] = useState(props.value);
-  const [calculatedCurrency, setCalculatedCurrency] = useState('________');
-  const [currency, setCurrency] = useState();
-  const [dataCurrencies, setDataCurrencies] = useState();
+    //значение, которое выводится
+    const [currency, setCurrency] = React.useState();
+    //значение, которое вводим
+    const [userValue, setUserValue] = React.useState(props.value);
+    //математические вычисления
+    const [calculatedCurrency, setCalculatedCurrency] = React.useState('________');
+    //запросы из сервера
+    const [dataCurrencies, setDataCurrencies] = React.useState();
   
 
     useEffect(() => {
-      fetch('https://www.cbr-xml-daily.ru/daily_json.js')
-                        .then(response => response.json())
-                        .then(json => setDataCurrencies(json));
-  }, []);
+        fetch('https://www.cbr-xml-daily.ru/daily_json.js')
+            .then(response => response.json())
+            .then(json => setDataCurrencies(json));
+    }, []);
   
-  useEffect(() => {
-    onCurrency(currency);
-  }, [userValue]);
+    useEffect(() => {
+        onCurrency(currency);
+    }, [userValue]);
   
-  function onCurrency(currency) {
-    if (currency && currency.length === 3) {
-      setCalculatedCurrency((userValue / dataCurrencies.Valute[currency].Value).toFixed(2));
-      setCurrency(currency);
+    function onCurrency(currency) {
+        if (currency && currency.length === 3) {
+        setCalculatedCurrency((userValue / dataCurrencies.Valute[currency].Value).toFixed(2));
+        setCurrency(currency);
+        }
     }
-  }
   
     function onUserValue(e) {
-    setUserValue(event.target.value);
-
-  }
+        setUserValue(event.target.value);
+    }
   
-  return (
-    <div class="app">
-      <input type="number" placeholder="Введите сумму в рубдях" class="user-input" onChange={(event) => onUserValue(event.target.value)} />
-      <div class="text">Вы ввели {userValue} RUR</div>
-      <div class="counter"><span>А это:</span>{calculatedCurrency} {currency}</div>
-      <div class="controls" onClick={(event) => onCurrency(event.target.innerText)}>
-        <button>USD</button>
-        <button>EUR</button>
-        <button>AUD</button>
-        <button>GBP</button>
-      </div>
-    </div>
-  )
+    return (
+        <div class="app">
+        <input type="number" placeholder="Введите сумму в рублях" class="user-input" onChange={(event) => onUserValue(event.target.value)} />
+        <div class="text">Вы ввели {userValue} RUB</div>
+        <div class="counter"><span>А это:</span>{calculatedCurrency} {currency}</div>
+        <div class="controls" onClick={(event) => onCurrency(event.target.innerText)}>
+            <button>USD</button>
+            <button>EUR</button>
+            <button>AUD</button>
+            <button>GBP</button>
+        </div>
+        </div>
+    )
 
 }
 
