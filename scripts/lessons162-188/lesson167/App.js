c27e15a12f43c2836dbe163c35cf75d99646f6b1
Vladislav <vladozdeveloper@gmail.com>
2022-08-23 21:12:25 +0300

    added app.js for lesson 167

---
+++ scripts/lessons162-188/lesson167/App.js
@@ -0,0 +1,34 @@
+import {useRef} from 'react';
+import {Container} from 'react-bootstrap';
+import './App.css';
+
+const Form = () => {
+    const myRef = ?
+
+    const focusFirstTI = () => {
+        myRef.current.focus();
+    }
+
+    return (
+        <Container>
+            <form className="w-50 border mt-5 p-3 m-auto">
+                <div className="mb-3">
+                    <label htmlFor="exampleFormControlInput1" className="form-label">Email address</label>
+                    <input ref={myRef} type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
+                    </div>
+                    <div className="mb-3">
+                    <label htmlFor="exampleFormControlTextarea1" className="form-label">Example textarea</label>
+                    <textarea onClick={focusFirstTI} className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                </div>
+            </form>
+        </Container>
+    )
+}
+
+function App() {
+    return (
+        <Form/>
+    );
+}
+
+export default App;
