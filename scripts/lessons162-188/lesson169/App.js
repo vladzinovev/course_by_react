1302c7429455dd53697948033d0ff9a8e27f4bff
Vladislav <vladozdeveloper@gmail.com>
2022-08-24 23:21:09 +0300

    added validate for search number iin the input

--- scripts/lessons162-188/lesson169/App.js
+++ scripts/lessons162-188/lesson169/App.js
@@ -5,13 +5,29 @@ import './App.css';
 const Form = () => {
     const [text, setText] = useState('');
 
+    const validateInput=(text)=>{
+        if(text.search(/\d/)>=0){
+            return true
+        } else {
+            return false
+        }
+    }
+
+    const color = validateInput(text)? 'text-danger':null
+
     return (
         <Container>
             <form className="w-50 border mt-5 p-3 m-auto">
                 <div className="mb-3">
                     <input value={text} type="text" className="form-control" readOnly/>
                     <label htmlFor="exampleFormControlInput1" className="form-label mt-3">Email address</label>
-                    <input onChange={(e) => setText(e.target.value)} type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
+                    <input 
+                    onChange={(e) => setText(e.target.value)} 
+                    type="email" 
+                    value={text}
+                    className={`form-control ${color}`} 
+                    id="exampleFormControlInput1" 
+                    placeholder="name@example.com"/>
                     </div>
                     <div className="mb-3">
                     <label htmlFor="exampleFormControlTextarea1" className="form-label">Example textarea</label>
