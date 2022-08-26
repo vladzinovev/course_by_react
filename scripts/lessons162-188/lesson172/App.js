2702377a90fa74b1782034925669b1889a50b4a5
Vladislav <vladozdeveloper@gmail.com>
2022-08-26 23:34:39 +0300

    added useDeferredValue

--- scripts/lessons162-188/lesson172/App.js
+++ scripts/lessons162-188/lesson172/App.js
@@ -1,13 +1,17 @@
 import data from './data';
-import {useState, useMemo} from 'react';
+import {useState, useMemo, useDeferredValue, useTransition} from 'react';
 
 function App() {
     const [text, setText] = useState('');
     const [posts, setPosts] = useState(data);
+    //чтобы сначал вводили а потом был поиск (очередность рендера определяется) (действует сам)
+    const defferedValue = useDeferredValue(text);
+
+
 
     const filteredPosts = useMemo(() => {
-        return posts.filter(item => item.name.toLowerCase().includes(text));
-    }, [text]);
+        return posts.filter(item => item.name.toLowerCase().includes(defferedValue));
+    }, [defferedValue]);
 
     const onValueChange = (e) => {
         setText(e.target.value);
