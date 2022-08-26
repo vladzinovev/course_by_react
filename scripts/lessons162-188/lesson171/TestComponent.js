fb0f6ed0b1f7daa492bf41c38cf28569451115d5
Vladislav <vladozdeveloper@gmail.com>
2022-08-26 22:55:29 +0300

    added TestComponent.js

---
+++ scripts/lessons162-188/lesson171/TestComponent.js
@@ -0,0 +1,16 @@
+import {useState} from 'react';
+function TestComponent(){
+    const [count,setCount]=useState(0);
+    const [flag,setFlag]=useState(false);
+    function handleClick(){
+        setCount(c=>c+1);
+        setFlag(f=>!f);
+    }
+    return(
+        <div>
+            <button onClick={handleClick}>Next</button>
+            <h1 style={{color:flag? "blue" : "black"}}>{count}</h1>
+        </div>
+    );
+}
+export default TestComponent;
