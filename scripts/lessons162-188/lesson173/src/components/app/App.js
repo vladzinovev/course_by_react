efbdc49ca00597a0256d8fa2c44266b34a742bdd
Vladislav <vladozdeveloper@gmail.com>
2022-08-27 00:40:26 +0300

    added Switch, Router, Route

--- scripts/lessons162-188/lesson173/src/components/app/App.js
+++ scripts/lessons162-188/lesson173/src/components/app/App.js
@@ -1,3 +1,5 @@
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+
 import { useState } from "react";
 import AppHeader from "../appHeader/AppHeader";
 import RandomChar from "../randomChar/RandomChar";
@@ -18,25 +20,35 @@ const App = () => {
     }
 
     return (
-        <div className="app">
-            <AppHeader/>
-            <main>
-                {/* <ErrorBoundary>
-                    <RandomChar/>
-                </ErrorBoundary>
-                <div className="char__content">
-                    <ErrorBoundary>
-                        <CharList onCharSelected={onCharSelected}/>
-                    </ErrorBoundary>
-                    <ErrorBoundary>
-                        <CharInfo charId={selectedChar}/>
-                    </ErrorBoundary>
-                </div>
-                <img className="bg-decoration" src={decoration} alt="vision"/> */}
-                <AppBanner/>
-                <ComicsList/>
-            </main>
-        </div>
+        <Router>
+            <div className="app">
+                <AppHeader/>
+                <main>
+                    <Switch>
+                        <Route exact path="/">
+                            <ErrorBoundary>
+                                <RandomChar/>
+                            </ErrorBoundary>
+                            <div className="char__content">
+                                <ErrorBoundary>
+                                    <CharList onCharSelected={onCharSelected}/>
+                                </ErrorBoundary>
+                                <ErrorBoundary>
+                                    <CharInfo charId={selectedChar}/>
+                                </ErrorBoundary>
+                            </div>
+                            <img className="bg-decoration" src={decoration} alt="vision"/>
+                        </Route>
+                        <Route exact path="/comics">
+                            <AppBanner/>
+                            <ComicsList/>
+                        </Route>
+                    </Switch>
+                    
+
+                </main>
+            </div>
+        </Router>
     )
 }
 
