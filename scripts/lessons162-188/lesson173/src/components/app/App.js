8278fb799d9f1612bd682069f688c92ed47086c4
Vladislav <vladozdeveloper@gmail.com>
2022-08-27 00:54:04 +0300

    rewrited app.js

--- scripts/lessons162-188/lesson173/src/components/app/App.js
+++ scripts/lessons162-188/lesson173/src/components/app/App.js
@@ -1,24 +1,10 @@
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
-import { useState } from "react";
+import {MainPage, ComicsPage} from '../pages';
 import AppHeader from "../appHeader/AppHeader";
-import RandomChar from "../randomChar/RandomChar";
-import CharList from "../charList/CharList";
-import CharInfo from "../charInfo/CharInfo";
-import ComicsList from "../comicsList/ComicsList";
-import AppBanner from "../appBanner/AppBanner";
-import ErrorBoundary from "../errorBoundary/ErrorBoundary";
-
-import decoration from '../../resources/img/vision.png';
 
 const App = () => {
     
-    const [selectedChar, setChar] = useState(null);
-
-    const onCharSelected = (id) => {
-        setChar(id);
-    }
-
     return (
         <Router>
             <div className="app">
@@ -26,26 +12,12 @@ const App = () => {
                 <main>
                     <Switch>
                         <Route exact path="/">
-                            <ErrorBoundary>
-                                <RandomChar/>
-                            </ErrorBoundary>
-                            <div className="char__content">
-                                <ErrorBoundary>
-                                    <CharList onCharSelected={onCharSelected}/>
-                                </ErrorBoundary>
-                                <ErrorBoundary>
-                                    <CharInfo charId={selectedChar}/>
-                                </ErrorBoundary>
-                            </div>
-                            <img className="bg-decoration" src={decoration} alt="vision"/>
+                            <MainPage/>
                         </Route>
                         <Route exact path="/comics">
-                            <AppBanner/>
-                            <ComicsList/>
+                            <ComicsPage/>
                         </Route>
                     </Switch>
-                    
-
                 </main>
             </div>
         </Router>
