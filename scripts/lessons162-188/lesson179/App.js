09b66c691221faf47885300c78848712d698fdb3
Vladislav <vladozdeveloper@gmail.com>
2022-09-01 22:16:32 +0300

    completed lesson

--- scripts/lessons162-188/lesson179/App.js
+++ scripts/lessons162-188/lesson179/App.js
@@ -1,10 +1,26 @@
-import {useState} from 'react';
+import {useState, useReducer} from 'react';
 import {Container} from 'react-bootstrap';
 import './App.css';
 
+//state-состояние, которое будет меняться (autoplay) до его изменения, action-название действия, которое хотим совершить
+function reducer (state, action) {
+    switch (action.type){
+        case 'toggle':
+            return {autoplay: !state.autoplay};
+        case 'toggle':
+            return {autoplay: 300};
+        case 'toggle':
+            return {autoplay: 700};
+        default:
+            throw new Error();
+    }
+}
+
 const Slider = () => {
     const [slide, setSlide] = useState(0);
-    const [autoplay, setAutoplay] = useState(false);
+    //const [autoplay, setAutoplay] = useState(false);
+    //первая часть наш стейт, а вторая через которую будет меняться наш стейт
+    const [autoplay, dispatch] = useReducer(reducer, {autoplay:false});
 
     function changeSlide(i) {
         setSlide(slide => slide + i);
@@ -14,7 +30,7 @@ const Slider = () => {
         <Container>
             <div className="slider w-50 m-auto">
                 <img className="d-block w-100" src="https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg" alt="slide" />
-                <div className="text-center mt-5">Active slide {slide} <br/>{autoplay ? 'auto' : null} </div>
+                <div className="text-center mt-5">Active slide {slide} <br/>{autoplay.autoplay ? 'auto' : null} </div>
                 <div className="buttons mt-3">
                     <button 
                         className="btn btn-primary me-2"
@@ -24,7 +40,13 @@ const Slider = () => {
                         onClick={() => changeSlide(1)}>+1</button>
                     <button 
                         className="btn btn-primary me-2"
-                        onClick={() => setAutoplay(!autoplay)}>toggle autoplay</button>
+                        onClick={() => dispatch({type:'toggle'})}>toggle autoplay</button>
+                    <button 
+                        className="btn btn-primary me-2"
+                        onClick={() => dispatch({type:'slow'})}>slow autoplay</button>
+                    <button 
+                        className="btn btn-primary me-2"
+                        onClick={() => dispatch({type:'fast'})}>fast autoplay</button>
                 </div>
             </div>
         </Container>
