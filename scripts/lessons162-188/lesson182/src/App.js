511226238ae2e9235ab1125b13ccce31bb6aba89
Vladislav <vladozdeveloper@gmail.com>
2022-09-02 23:01:45 +0300

    added onEnter and onExited

--- scripts/lessons162-188/lesson182/src/App.js
+++ scripts/lessons162-188/lesson182/src/App.js
@@ -8,19 +8,24 @@ const Modal = (props) => {
     const duration = 300;
 
     const defaultStyle = {
-        transition: `opacity ${duration}ms ease-in-out`,
+        transition: `opacity, visibility ${duration}ms ease-in-out`,
         opacity: 0,
+        visibility:'hidden'
     }
 
     const transitionStyles = {
-        entering: { opacity: 1 },
-        entered:  { opacity: 1 },
-        exiting:  { opacity: 0 },
-        exited:  { opacity: 0 },
+        entering: { opacity: 1, visibility:'visible' },
+        entered:  { opacity: 1, visibility:'visible' },
+        exiting:  { opacity: 0, visibility:'hidden' },
+        exited:  { opacity: 0, visibility:'hidden' },
     };
 
     return (
-        <Transition in={props.show} timeout={duration}>
+        <Transition 
+            in={props.show} 
+            timeout={duration}
+            onEnter={()=>props.setShowTrigger(false)}
+            onExited={()=>props.setShowTrigger(true)}>
             {state=>(
                 <div className="modal mt-5 d-block" style={{
                     ...defaultStyle,
@@ -49,14 +54,16 @@ const Modal = (props) => {
 
 function App() {
     const [showModal, setShowModal] = useState(false);
+    const [showTrigger, setShowTrigger]=useState(true);
 
     return (
         <Container>
-            <Modal show={showModal} onClose={setShowModal}/>
-            <button 
+            <Modal show={showModal} onClose={setShowModal} setShowTrigger={setShowTrigger}/>
+            {showTrigger ? 
+                <button 
                 type="button" 
                 className="btn btn-warning mt-5"
-                onClick={() => setShowModal(true)}>Open Modal</button>
+                onClick={() => setShowModal(true)}>Open Modal</button> : null}
         </Container>
     );
 }
