891ddaeb60da970fc8f5fa056cdb86d5537fc1e7
Vladislav <vladozdeveloper@gmail.com>
2022-09-08 23:41:29 +0300

    added React component in the project

--- scripts/lessons189-213/lesson193/src/index.js
+++ scripts/lessons189-213/lesson193/src/index.js
@@ -4,50 +4,28 @@ import './index.css';
 import { createStore } from 'redux';
 import reducer from './reducer';
 import {inc, dec, rnd} from './actions';
+import { Provider } from 'react-redux';
 
 
 const store = createStore(reducer);
 const {dispatch, subscribe, getState} = store;
 
-const update = () => {
-    //document.getElementById('counter').textContent = store.getState().value;
-    document.getElementById('counter').textContent = getState().value;
-}
+/* const update = () => {} */
 
 //subscribe выполняет действие после каждого измененения state
-//store.subscribe(update);
+
 subscribe(update);
 
 
-/* сделали сами
-const bindActionCreator=(creator,dispatch)=>(...args)=>{
-    dispatch(creator(...args));
-} 
-const incDispatch=bindActionCreator(inc,dispatch);
-const decDispatch=bindActionCreator(dec,dispatch);
-const rndDispatch=bindActionCreator(rnd,dispatch);
-*/
 
-/* через паттерн
-const incDispatch=bindActionCreators(inc,dispatch);
-const decDispatch=bindActionCreators(dec,dispatch);
-const rndDispatch=bindActionCreators(rnd,dispatch); */
-
-//сокращение
 const {incDispatch, decDispatch,rndDispatch }=bindActionCreators({
     incDispatch:inc,
     decDispatch:dec,
     rndDispatch:rnd
 }, dispatch);
 
-//было
-/* document.getElementById('inc').addEventListener('click', () => {
-    //store.dispatch(inc());
-    dispatch(inc());
-}); */
-//стало
-document.getElementById('inc').addEventListener('click', incDispatch);
 
+document.getElementById('inc').addEventListener('click', incDispatch);
 
 document.getElementById('dec').addEventListener('click', decDispatch);
 
@@ -61,8 +39,18 @@ document.getElementById('rnd').addEventListener('click', () => {
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <>
+    {/* <Counter
+        counter={getState().value}
+        inc={incDispatch}
+        dec={decDispatch}
+        rnd={()=>{
+            const value = Math.floor(Math.random() * 10);
+            rndDispatch(value);
+        }}
+    /> */}
+    <Provider store={store}>
+        <App/>
+    </Provider>
     
-    </>
   </React.StrictMode>
 );
