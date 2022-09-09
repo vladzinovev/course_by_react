7c285d9051ba825682e93e56b975f6b19a881625
Vladislav <vladozdeveloper@gmail.com>
2022-09-09 21:47:46 +0300

    completed lesson

--- scripts/lessons189-213/lesson194/src/components/Counter.js
+++ scripts/lessons189-213/lesson194/src/components/Counter.js
@@ -1,30 +1,53 @@
+import { Component } from "react";
 import { connect } from "react-redux";
-import {inc, dec, rnd} from '../actions';
+import * as actions from '../actions';
+import {bindActionCreators} from "redux";
 
-const Counter = (counter, inc, dec, rnd)=>{
-    return (
-        <div class="jumbotron">
-            <h1>{counter}</h1>
-            <button onClick={dec} className="btn btn-primary">DEC</button>
-            <button onClick={inc} className="btn btn-primary">INC</button>
-            <button onClick={rnd} className="btn btn-primary">RND</button>
-        </div>
-    )
+// const Counter = ({counter, inc, dec, rnd}) => {
+//     return (
+//         <div className="jumbotron">
+//             <h1>{counter}</h1>
+//             <button onClick={dec} className="btn btn-primary">DEC</button>
+//             <button onClick={inc} className="btn btn-primary">INC</button>
+//             <button onClick={rnd} className="btn btn-primary">RND</button>
+//         </div>
+//     )
+// }
+
+class Counter extends Component {
+    render() {
+        const {counter, inc, dec, rnd} = this.props;
+        return (
+            <div className="jumbotron">
+                <h1>{counter}</h1>
+                <button onClick={dec} className="btn btn-primary">DEC</button>
+                <button onClick={inc} className="btn btn-primary">INC</button>
+                <button onClick={rnd} className="btn btn-primary">RND</button>
+            </div>
+        )
+    }
 }
 const mapStateToProps = (state)=>{
     return{
         counter:state.value
     }
+} 
+
+const mapDispatchToProps=(dispatch)=>{
+    /* const {inc,dec,rnd}=bindActionCreators(actions,dispatch);
+    return{
+        //inc:inc,
+        //или просто
+        inc,
+
+        dec,
+        rnd:()=>{
+            ;
+            rnd(value);
+        } //или просто
+        rnd
+    } *///или просто
+    return bindActionCreators(actions,dispatch);
 }
 
-const mapDispatchToProps=(incDispatch, decDispatch,rndDispatch)=>{
-    return{
-        inc:()=>incDispatch(),
-        dec:()=>decDispatch(),
-        rnd:()=>{
-            const value = Math.floor(Math.random() * 10);
-            rndDispatch(value);
-    }
-        }
-}
-export default connect(mapStateToProps, mapDispatchToProps)(Counter);
+export default connect(mapStateToProps, actions)(Counter);
